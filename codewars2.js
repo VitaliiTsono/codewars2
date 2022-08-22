ffb21c4a2bc95f6e8b71a8832e0218e5a8591957
VitaliiTsono <104579923+VitaliiTsono@users.noreply.github.com>
2022-08-22 16:20:51 +0300

    Create codewars2.js

---
+++ codewars2.js
@@ -0,0 +1,66 @@
+1)
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+   args.sort((a, b) => a - b)
+   return args[0];
+}
+  }
+
+
+2)
+
+function circleCircumference(circle) {
+ return (circle.radius * 2 * Math.PI );
+
+}
+
+
+3)
+
+function giveMeFive(obj){
+  var f = [];
+  for (var key in obj) {
+    if (key.length === 5) f.push(key);
+    if (obj[key].length === 5) f.push(obj[key]);
+  }
+  return f;
+}
+  
+4)
+
+function buildFun(n){
+  var bF = [];
+  for (let i = 0; i< n; i++){
+    bF.push(() => i);
+  }
+  return bF;
+}
+
+
+5)
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce() {
+    return super.introduce() + "  Meow meow!";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
